e63db6750a72d374bf944391343ccfd0acbf585b
Adam Bartonicek <bartonicek@gmail.com>
2022-11-07 11:15:27 +1300

    Cleaned up some logic on Plot

--- src/plot/Plot.ts
+++ src/plot/Plot.ts
@@ -6,7 +6,6 @@ import * as auxs from "../auxiliaries/auxiliaries.js";
 import * as hndl from "../handlers/handlers.js";
 import { GraphicStack } from "./GraphicStack.js";
 import { Wrangler } from "../wrangler/Wrangler.js";
-import { DataFrame } from "../DataFrame.js";
 
 export class Plot extends GraphicStack {
   id: string;
@@ -28,13 +27,12 @@ export class Plot extends GraphicStack {
   constructor(plotConfig: {
     id: string;
     element: HTMLDivElement;
-    data: DataFrame;
+    data: dtstr.DataFrame;
     mapping: dtstr.Mapping;
     globals: dtstr.Globals;
-    dimensions?: { width: number; height: number };
   }) {
-    const { id, element, data, mapping, globals, dimensions } = plotConfig;
-    super(element, globals, dimensions);
+    const { id, element, mapping, globals } = plotConfig;
+    super(element);
     this.id = id;
     this.representations = {};
     this.wranglers = {};
@@ -43,85 +41,71 @@ export class Plot extends GraphicStack {
       marker: globals.marker,
       keypress: globals.keypress,
       state: globals.state,
-      drag: new hndl.DragHandler(this.graphicContainer),
-      click: new hndl.ClickHandler(this.graphicContainer),
+      drag: new hndl.DragHandler(this.containerDiv),
+      click: new hndl.ClickHandler(this.containerDiv),
     };
     this.auxiliaries = {
       axisbox: new auxs.AxisBox(),
-      axistextx: new auxs.AxisText("x"),
-      axistexy: new auxs.AxisText("y"),
-      axistitlex: new auxs.AxisTitle("x", mapping.get("x")),
-      axistitley: new auxs.AxisTitle("y", mapping.get("y")),
+      axistextx: new auxs.AxisText("x", this),
+      axistexy: new auxs.AxisText("y", this),
+      axistitlex: new auxs.AxisTitle("x", mapping.get("x"), this),
+      axistitley: new auxs.AxisTitle("y", mapping.get("y"), this),
       highlightrects: new auxs.HighlightRects(this.handlers),
     };
+  }
+
+  get active() {
+    return this.handlers.state.isActive(this.id);
+  }
+
+  get fontsize() {
+    return Math.floor(Math.min(this.width, this.height) * 0.05);
   }
 
   resize = () => {
     const graphicLayers = ["graphicBase", "graphicUser", "graphicHighlight"];
     this.scales.x.setLength(this.width);
     this.scales.y.setLength(this.height);
-    this.graphicContainer.style.width = `${this.width}px`;
-    this.graphicContainer.style.height = `${this.height}px`;
     graphicLayers.forEach((e) => this[e].resize());
   };
-
-  get active() {
-    return this.handlers.state.isActive(this.id);
-  }
 
   activate = () => {
     this.handlers.state.deactivateAll();
     this.handlers.state.activate(this.id);
   };
 
-  // Calls a method (string) on each child of a property (string)
-  // e.g. call method "drawBase" on each child of "representations"
-  callChildren = (object: keyof this, fun: string, ...args: any[]) => {
-    const obj = this[object];
-    Object.keys(obj).forEach((child) => {
-      obj[child][fun] ? obj[child][fun](...args) : null;
-    });
-  };
-
-  // Returns a result of a function [string] from each child of a property [string]
-  // e.g. return selected points from each representation
-  mapChildren = (object: keyof this, fun: string, ...args: any[]) => {
-    const obj = this[object];
-    return Object.keys(obj).map((child) => {
-      return obj[child][fun] ? obj[child][fun](...args) : null;
-    });
-  };
-
   // Gets all unique values of a mapping [string], across all wranglers
   getUnique = (mapping: string) => {
-    const { wranglers } = this;
-    const arr = Object.keys(wranglers).flatMap(
-      (name) => wranglers[name][mapping].extract() ?? []
+    const arr = Object.keys(this.wranglers).map((name) =>
+      this.wranglers[name][mapping]?.extract()
     );
-    return Array.from(new Set(arr));
+    return Array.from(new Set(arr.flat()));
   };
 
   // Given an array of selection points, checks each representation
-  inSelection = (selPoints: [number, number][]): number[] => {
-    const { mapChildren } = this;
-    const allPoints = mapChildren("representations", "inSelection", selPoints);
+  inSelection = (selPoints: dtstr.Rect2Points): number[] => {
+    const allPoints = Object.keys(this.representations).map((e) => {
+      return this.representations[e]?.inSelection?.(selPoints);
+    });
     return Array.from(new Set(allPoints.flat()));
   };
 
   inClickPosition = (clickPoint: [number, number]): number[] => {
-    const { mapChildren } = this;
-    const allPoints = mapChildren("representations", "atClick", clickPoint);
+    const allPoints = Object.keys(this.representations).map((e) => {
+      return this.representations[e]?.atClick?.(clickPoint);
+    });
     return Array.from(new Set(allPoints.flat()));
   };
 
-  startDrag = () => {
-    const { marker, state, drag } = this.handlers;
-    const { highlightrects } = this.auxiliaries;
+  updateCurrent = () => this.drawHighlight();
+  clearAll = () => this.drawHighlight();
 
-    if (!state.inState("none") && highlightrects.lastComplete) {
+  startDrag = () => {
+    const { state, drag } = this.handlers;
+    const { highlightrects } = this.auxiliaries;
+    if (!state.none && highlightrects.lastComplete) {
       highlightrects.pushLastToPast();
     }
-
     highlightrects.updateCurrentOrigin(drag.start);
   };
 
@@ -131,69 +115,66 @@ export class Plot extends GraphicStack {
 
     highlightrects.updateCurrentEndpoint(drag.end);
     highlightrects.updateLast();
-    this.drawUser();
     marker.updateCurrent(
       this.inSelection([drag.start, drag.end]),
       state.membership
     );
+    if (this.active || state.none) this.draw("user");
   };
 
   endDrag = () => {
-    const { marker, state } = this.handlers;
     const { highlightrects } = this.auxiliaries;
-    marker.mergeCurrent();
-    if (!state.inState("none") && highlightrects.lastComplete) {
+    if (!this.handlers.state.none && highlightrects.lastComplete) {
       highlightrects.pushLastToPast();
     }
   };
 
-  onKeypress = () => {
-    const { callChildren, drawHighlight } = this;
-    const { keypress } = this.handlers;
-    callChildren("handlers", "onKeypress", keypress.lastPressed);
+  keyPressed = (key: string) => {
     if (this.active) {
-      callChildren("representations", "onKeypress", keypress.lastPressed);
-      drawHighlight();
+      Object.keys(this.representations).forEach((e) => {
+        this.representations[e].keyPressed(key);
+      });
+      this.drawBase();
+      this.drawHighlight();
     }
   };
 
-  onKeyRelease = () => {
-    const { handlers, callChildren } = this;
-    callChildren("handlers", "onKeyRelease", handlers.keypress.lastPressed);
-    if (this.active) {
-      callChildren(
-        "representations",
-        "onKeyRelease",
-        handlers.keypress.lastPressed
-      );
-    }
-  };
+  keyReleased = () => {};
 
-  onMouseDownAnywhere = () => {
-    const { marker, state } = this.handlers;
-    if (state.inState("none")) {
+  mouseDownAnyPlot = (event: MouseEvent) => {
+    event.cancelBubble = true;
+    event.stopPropagation?.();
+    if (this.handlers.state.none) {
       this.auxiliaries.highlightrects.clear();
       this.drawUser();
     }
   };
 
-  onMouseDownHere = () => {
-    const { marker, click, drag, state } = this.handlers;
-    const { highlightrects } = this.auxiliaries;
-    if (state.inState("none")) {
+  mouseDownThisPlot = (event: MouseEvent) => {
+    const { marker, click, state } = this.handlers;
+
+    event.cancelBubble = true;
+    event.stopPropagation?.();
+
+    marker.mergeCurrent(state.membership === 128);
+
+    if (state.none) {
       marker.clearCurrent();
       this.auxiliaries.highlightrects.clear();
+      this.drawUser();
     }
+
     state.deactivateAll();
     this.activate();
+
     marker.updateCurrent(
       this.inClickPosition(click.clickLast),
       state.membership
     );
   };
 
-  onDoubleClick = () => {
-    const { marker, drag, state } = this.handlers;
+  doubleClick = () => {
+    const { marker, state } = this.handlers;
     state.activateAll();
     marker.clearAll();
     this.auxiliaries.highlightrects.clear();
@@ -203,20 +184,25 @@ export class Plot extends GraphicStack {
   };
 
   draw = (context: "base" | "highlight" | "user", ...args: any[]) => {
-    const { representations, auxiliaries, callChildren } = this;
-    const what = "draw" + funs.capitalize(context);
-    const where = "graphic" + funs.capitalize(context);
+    const { representations, auxiliaries } = this;
+
+    const [what, where] = [
+      "draw" + funs.capitalize(context),
+      "graphic" + funs.capitalize(context),
+    ];
     if (context !== "user") this[where].drawClear();
     if (context === "base") this[where].drawBackground();
-    callChildren("representations", what, this[where], ...args);
-    callChildren("auxiliaries", what, this[where], ...args);
+
+    const repsAndAuxs = { ...representations, ...auxiliaries };
+    Object.keys(repsAndAuxs).forEach((e) => {
+      repsAndAuxs[e]?.[what]?.(this[where], ...args);
+    });
   };
 
   drawBase = () => this.draw("base");
   drawHighlight = () => this.draw("highlight");
-  drawUser = () => {
-    if (this.active || this.handlers.state.inState("none")) this.draw("user");
-  };
+  drawUser = () => this.draw("user");
+
   drawRedraw = () => {
     this.drawBase();
     this.drawHighlight();
@@ -225,52 +211,35 @@ export class Plot extends GraphicStack {
 
   initialize = () => {
     const {
+      representations,
+      auxiliaries,
       handlers,
       scales,
-      callChildren,
-      onMouseDownHere,
-      onMouseDownAnywhere,
-      onDoubleClick,
-      onKeypress,
-      onKeyRelease,
+      mouseDownThisPlot: mouseDownHere,
+      mouseDownAnyPlot: mouseDownAnywhere,
+      doubleClick,
       drawBase,
-      drawHighlight,
-      drawUser,
-      startDrag,
-      whileDrag,
-      endDrag,
-      graphicContainer,
-      graphicDiv,
+      containerDiv,
+      sceneDiv,
     } = this;
-
-    Object.keys(scales).forEach((mapping) => {
-      scales[mapping]?.registerData(this.getUnique(mapping));
-    });
-
-    callChildren("representations", "registerScales", scales);
-    callChildren("auxiliaries", "registerScales", scales);
 
     this.handlers.drag.state = this.handlers.state;
 
-    graphicDiv.addEventListener("dblclick", onDoubleClick);
-    graphicDiv.addEventListener("mousedown", onMouseDownAnywhere);
-    graphicContainer.addEventListener("mousedown", onMouseDownHere);
+    Object.keys(scales).forEach((e) => {
+      scales[e].registerData?.(this.getUnique(e));
+    });
 
-    handlers.marker.registerCallbacks(
-      [drawHighlight, drawHighlight],
-      ["updateCurrent", "clearAll"]
-    );
-    handlers.state.registerCallbacks([drawUser], ["deactivateAll"]);
-
-    handlers.drag.registerCallbacks(
-      [startDrag, whileDrag, endDrag],
-      ["startDrag", "whileDrag", "endDrag"]
+    const repsAndAuxs = { ...representations, ...auxiliaries };
+    Object.keys(repsAndAuxs).forEach((e) =>
+      repsAndAuxs[e].registerScales?.(scales)
     );
 
-    handlers.keypress.registerCallbacks(
-      [onKeypress, onKeyRelease, drawBase],
-      ["keyPressed", "keyReleased", "keyPressed"]
-    );
+    sceneDiv.addEventListener("dblclick", doubleClick);
+    sceneDiv.addEventListener("mousedown", mouseDownAnywhere);
+    containerDiv.addEventListener("mousedown", mouseDownHere);
+
+    Object.keys(handlers).forEach((e) => handlers[e].subscribe(this));
+
     drawBase();
   };
 }
